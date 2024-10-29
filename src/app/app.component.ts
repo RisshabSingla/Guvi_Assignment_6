eab24a3747ded46b38dcc90e187da0325fd14a52
Risshab Singla <risshab2singla@gmail.com>
2024-10-29 22:28:00 +0530

    Footer Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { HeaderComponent } from './header/header.component';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
 import { FeaturesSectionComponent } from './features-section/features-section.component';
 import { CallToActionComponent } from './call-to-action/call-to-action.component';
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, HeroSectionComponent, FeaturesSectionComponent, CallToActionComponent],
+  imports: [HeaderComponent, HeroSectionComponent, FeaturesSectionComponent, CallToActionComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
