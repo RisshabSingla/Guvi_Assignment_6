110be8a67df635217b25c15dfa3103195d2fe0ce
Risshab Singla <risshab2singla@gmail.com>
2024-10-29 22:20:42 +0530

    Call to Action Component Completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
 import { FeaturesSectionComponent } from './features-section/features-section.component';
+import { CallToActionComponent } from './call-to-action/call-to-action.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, HeroSectionComponent, FeaturesSectionComponent],
+  imports: [HeaderComponent, HeroSectionComponent, FeaturesSectionComponent, CallToActionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
