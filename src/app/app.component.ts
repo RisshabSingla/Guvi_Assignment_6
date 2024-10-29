e72ea55a0e4012659fbe8093cb1238c0d1ce7819
Risshab Singla <risshab2singla@gmail.com>
2024-10-29 18:27:51 +0530

    Hero Section Completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, HeroSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
